640033c030a7ed20f6a81ef463c679fe68001004
hayashitoshiki <tosikihayasi@outlook.jp>
2021-11-14 23:48:44 +0900

    ライフサイクルサンプル追加

--- app/src/main/java/com/myapp/composesample/ui/left/FirstScreen.kt
+++ app/src/main/java/com/myapp/composesample/ui/left/FirstScreen.kt
@@ -1,6 +1,7 @@
 package com.myapp.composesample.ui.left
 
 import android.content.res.Configuration
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
@@ -12,16 +13,21 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.myapp.composesample.R
@@ -38,6 +44,7 @@ fun FirstScreen(viewModel: FirstViewModel, navController: NavHostController) {
 
     // effect
     LaunchedEffect(true) {
+        Log.d("Lifecycle", "LaunchedEffect")
         viewModel.effect.onEach { effect ->
             when (effect) {
                 is FirstContract.Effect.NavigateToButtonGroupScreen -> {
@@ -88,6 +95,29 @@ fun FirstScreen(viewModel: FirstViewModel, navController: NavHostController) {
             }
         }.collect()
     }
+
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val observer = remember {
+        LifecycleEventObserver {_, event ->
+            when(event) {
+                Lifecycle.Event.ON_CREATE -> { Log.d("Lifecycle", ">>>>>>>>>>>>ON_CREATE")}
+                Lifecycle.Event.ON_START -> { Log.d("Lifecycle", ">>>>>>>>>>>>>ON_START")}
+                Lifecycle.Event.ON_RESUME -> { Log.d("Lifecycle", ">>>>>>>>>>>>>>ON_RESUME")}
+                Lifecycle.Event.ON_PAUSE -> { Log.d("Lifecycle", "<<<<<<<<<<<<<<ON_PAUSE")}
+                Lifecycle.Event.ON_STOP -> { Log.d("Lifecycle", "<<<<<<<<<<<<<ON_STOP")}
+                Lifecycle.Event.ON_DESTROY -> { Log.d("Lifecycle", "<<<<<<<<<<<<ON_DESTROY")}
+                Lifecycle.Event.ON_ANY -> { Log.d("Lifecycle", "ON_ANY")}
+            }
+        }
+    }
+    DisposableEffect(Unit) {
+        lifecycle.addObserver(observer)
+        onDispose {
+            Log.d("Lifecycle", "DisposableEffect")
+            lifecycle.removeObserver(observer)
+        }
+    }
+
 
     // event
     val navigateToTextGroupScreen: () -> Unit = {
