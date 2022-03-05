f20f3a96e549d12332e2d7f4159ebf7dc0c9ca29
hayashitoshiki <tosikihayasi@outlook.jp>
2022-03-05 13:36:57 +0900

    カスタムタブバーサンプル作成

--- app/src/main/java/com/myapp/composesample/ui/right/ThirdScreen.kt
+++ app/src/main/java/com/myapp/composesample/ui/right/ThirdScreen.kt
@@ -1,13 +1,16 @@
 package com.myapp.composesample.ui.right
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.myapp.composesample.util.component.Sample1TabComponent
 
@@ -26,7 +29,9 @@ fun ThirdScreen() {
             modifier = Modifier
                 .fillMaxWidth(),
         ) {
-            Sample1TabComponent()
+            Box(modifier = Modifier.padding(top = 16.dp)){
+                Sample1TabComponent()
+            }
         }
     }
 }
--- app/src/main/java/com/myapp/composesample/util/component/TabComponent.kt
+++ app/src/main/java/com/myapp/composesample/util/component/TabComponent.kt
@@ -4,14 +4,20 @@ package com.myapp.composesample.util.component
 // 　　　　　TabコンテンツComponent
 // ========================================
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.pager.*
@@ -19,6 +25,9 @@ import kotlinx.coroutines.launch
 import com.myapp.composesample.ui.right.Tab1Screen
 import com.myapp.composesample.ui.right.Tab2Screen
 import com.myapp.composesample.ui.right.Tab3Screen
+import com.myapp.composesample.util.Extension.SealedClassEnumExtension
+import com.myapp.composesample.util.Extension.SealedClassEnumWithName
+import com.myapp.composesample.util.Extension.values
 
 
 // ======================================== //
@@ -37,43 +46,71 @@ abstract class TabItem(val icon: ImageVector, val title: String, val screen: @Co
 /**
  * サンプル１タブ
  */
-sealed class Sample1TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit) : TabItem(icon, title, screen){
+sealed class Sample1TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit)
+    : TabItem(icon, title, screen), SealedClassEnumWithName {
     @ExperimentalPagerApi
-    object Music : Sample1TabItem(Icons.Filled.Home, "Text Only", { Sample2TabComponent() })
+    object TextIcon : Sample1TabItem(Icons.Filled.Home, "Text & Icon", { Sample2TabComponent() })
     @ExperimentalPagerApi
-    object Movies : Sample1TabItem(Icons.Filled.ShoppingCart, "Icon Only", { Sample3TabComponent() })
+    object Text : Sample1TabItem(Icons.Filled.Home, "Text Only", { Sample3TabComponent() })
     @ExperimentalPagerApi
-    object Books : Sample1TabItem(Icons.Filled.Settings, "Scroll", { Sample4TabComponent() })
+    object Icon : Sample1TabItem(Icons.Filled.ShoppingCart, "Icon Only", { Sample4TabComponent() })
+    @ExperimentalPagerApi
+    object Scroll : Sample1TabItem(Icons.Filled.Settings, "Scroll", { Sample5TabComponent() })
+
+    companion object : SealedClassEnumExtension<Sample1TabItem>
 }
 
 /**
  * サンプル２タブ
  */
-sealed class Sample2TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit) : TabItem(icon, title, screen){
+sealed class Sample2TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit)
+    : TabItem(icon, title, screen), SealedClassEnumWithName {
     object Tab1 : Sample2TabItem(Icons.Filled.Home, "Home", { Tab1Screen() })
     object Tab2 : Sample2TabItem(Icons.Filled.ShoppingCart, "Shopp", { Tab2Screen() })
     object Tab3 : Sample2TabItem(Icons.Filled.Settings, "Setting", { Tab3Screen() })
+
+    companion object : SealedClassEnumExtension<Sample2TabItem>
 }
+
 /**
  * サンプル３タブ
  */
-sealed class Sample3TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit) : TabItem(icon, title, screen){
+sealed class Sample3TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit)
+    : TabItem(icon, title, screen), SealedClassEnumWithName {
     object Tab1 : Sample3TabItem(Icons.Filled.Home, "Home", { Tab1Screen() })
     object Tab2 : Sample3TabItem(Icons.Filled.ShoppingCart, "Shopp", { Tab2Screen() })
     object Tab3 : Sample3TabItem(Icons.Filled.Settings, "Setting", { Tab3Screen() })
+
+    companion object : SealedClassEnumExtension<Sample3TabItem>
 }
 
 /**
  * サンプル４タブ
  */
-sealed class Sample4TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit) : TabItem(icon, title, screen){
+sealed class Sample4TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit)
+    : TabItem(icon, title, screen), SealedClassEnumWithName {
     object Tab1 : Sample4TabItem(Icons.Filled.Home, "Tab1", { Tab1Screen() })
     object Tab2 : Sample4TabItem(Icons.Filled.ShoppingCart, "Tab2", { Tab2Screen() })
     object Tab3 : Sample4TabItem(Icons.Filled.Settings, "Tab3", { Tab3Screen() })
-    object Tab4 : Sample4TabItem(Icons.Filled.Home, "Tab4", { Tab1Screen() })
-    object Tab5 : Sample4TabItem(Icons.Filled.ShoppingCart, "Tab5", { Tab2Screen() })
-    object Tab6 : Sample4TabItem(Icons.Filled.Settings, "Tab6", { Tab3Screen() })
+
+    companion object : SealedClassEnumExtension<Sample4TabItem>
 }
+
+/**
+ * サンプル５タブ
+ */
+sealed class Sample5TabItem(icon: ImageVector, title: String, screen: @Composable () -> Unit)
+    : TabItem(icon, title, screen), SealedClassEnumWithName {
+    object Tab1 : Sample5TabItem(Icons.Filled.Home, "Tab1", { Tab1Screen() })
+    object Tab2 : Sample5TabItem(Icons.Filled.ShoppingCart, "Tab2", { Tab2Screen() })
+    object Tab3 : Sample5TabItem(Icons.Filled.Settings, "Tab3", { Tab3Screen() })
+    object Tab4 : Sample5TabItem(Icons.Filled.Home, "Tab4", { Tab1Screen() })
+    object Tab5 : Sample5TabItem(Icons.Filled.ShoppingCart, "Tab5", { Tab2Screen() })
+    object Tab6 : Sample5TabItem(Icons.Filled.Settings, "Tab6", { Tab3Screen() })
+
+    companion object : SealedClassEnumExtension<Sample5TabItem>
+}
+
 // ========================================  //
 // 　　　　　公開用タブ画面Component             //
 // ======================================== //
@@ -86,14 +123,15 @@ sealed class Sample4TabItem(icon: ImageVector, title: String, screen: @Composabl
 @Composable
 fun Sample1TabComponent() {
     val pagerState = rememberPagerState(initialPage = 0)
-    val tabs = listOf(Sample1TabItem.Music, Sample1TabItem.Movies, Sample1TabItem.Books)
+    val tabs = Sample1TabItem.values()
 
     // レイアウト
     Column {
-        TextIconTabBar(tabs = tabs, pagerState = pagerState)
+        CustomTabBar(tabs = tabs, pagerState = pagerState)
         TabsContent(tabs = tabs, pagerState = pagerState)
     }
 }
+
 /**
  * サンプルタブ画面Component
  *
@@ -102,11 +140,24 @@ fun Sample1TabComponent() {
 @Composable
 fun Sample2TabComponent() {
     val pagerState = rememberPagerState(initialPage = 0)
-    val tabs = listOf(
-        Sample2TabItem.Tab1,
-        Sample2TabItem.Tab2,
-        Sample2TabItem.Tab3
-    )
+    val tabs = Sample2TabItem.values()
+
+    // レイアウト
+    Column {
+        TextIconTabBar(tabs = tabs, pagerState = pagerState)
+        TabsContent(tabs = tabs, pagerState = pagerState)
+    }
+}
+
+/**
+ * サンプルタブ画面Component
+ *
+ */
+@ExperimentalPagerApi
+@Composable
+fun Sample3TabComponent() {
+    val pagerState = rememberPagerState(initialPage = 0)
+    val tabs = Sample3TabItem.values()
 
     // レイアウト
     Column {
@@ -121,13 +172,9 @@ fun Sample2TabComponent() {
  */
 @ExperimentalPagerApi
 @Composable
-fun Sample3TabComponent() {
+fun Sample4TabComponent() {
     val pagerState = rememberPagerState(initialPage = 0)
-    val tabs = listOf(
-        Sample3TabItem.Tab1,
-        Sample3TabItem.Tab2,
-        Sample3TabItem.Tab3
-    )
+    val tabs = Sample4TabItem.values()
 
     // レイアウト
     Column {
@@ -142,16 +189,9 @@ fun Sample3TabComponent() {
  */
 @ExperimentalPagerApi
 @Composable
-fun Sample4TabComponent() {
+fun Sample5TabComponent() {
     val pagerState = rememberPagerState(initialPage = 0)
-    val tabs = listOf(
-        Sample4TabItem.Tab1,
-        Sample4TabItem.Tab2,
-        Sample4TabItem.Tab3,
-        Sample4TabItem.Tab4,
-        Sample4TabItem.Tab5,
-        Sample4TabItem.Tab6
-    )
+    val tabs = Sample5TabItem.values()
 
     // レイアウト
     Column {
@@ -160,13 +200,12 @@ fun Sample4TabComponent() {
     }
 }
 
-
 // ======================================== //
 // 　　　　　内部用Util Tab Component          //
 // ======================================== //
 
 /**
- * タブバー
+ * Textタブバー
  *
  * @param tabs タブリスト
  * @param pagerState pager
@@ -289,6 +328,64 @@ private fun ScrollableTabBar(tabs: List<TabItem>, pagerState: PagerState) {
                 },
                 text = { Text(text = tab.title) }
             )
+        }
+    }
+}
+
+/**
+ * カスタムレイアウトタブバー
+ *
+ * @param tabs タブリスト
+ * @param pagerState pager
+ */
+@ExperimentalPagerApi
+@Composable
+private fun CustomTabBar(tabs: List<TabItem>, pagerState: PagerState) {
+    val coroutineScope = rememberCoroutineScope()
+
+    TabRow(
+        selectedTabIndex = pagerState.currentPage,
+        backgroundColor = Color.Transparent,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(Modifier.pagerTabIndicatorOffset(pagerState, tabPositions))
+        }
+    ) {
+        tabs.forEachIndexed { index, tab ->
+            val isSelected = pagerState.currentPage == index
+            val tabColor = if (isSelected) { Color.Red } else { Color.Transparent }
+            val roundedCornerShape = RoundedCornerShape(
+                topStart = if (index == 0){ 10.dp } else { 0.dp },
+                topEnd = if (index == tabs.size -1){ 10.dp } else { 0.dp }
+            )
+
+            Tab(
+                selected = isSelected,
+                onClick = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }
+            ) {
+                // この中でタブのレイアウトを作成
+                Box(
+                    modifier = Modifier
+                        .height(32.dp)
+                        .fillMaxWidth()
+                        .clip(roundedCornerShape)
+                        .background(color = tabColor)
+                        .border(
+                            width = 1.dp,
+                            color = Color.DarkGray,
+                            shape = roundedCornerShape
+                        )
+                ) {
+                    Text(
+                        text = tab.title,
+                        style = MaterialTheme.typography.body1,
+                        modifier = Modifier.align(Alignment.Center)
+                    )
+                }
+            }
         }
     }
 }
